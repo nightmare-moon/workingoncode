0abb381d91d7b932eef46da6606374fb61ff8d76
Andre Johnson <andre.johnson@gmail.com>
2022-01-04 04:17:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#class PlayerCharacter:
+#  membership = True
+#  def __init__(self, name, age):
+#    if (self.membership)
+#      self.name = name
+#      self.age = age
+  
+#  def run(self):
+#    print("run")
+
+#@classmethod
+#def 
+
+
+
+#player1 = PlayerCharacter("roxy",12 )
+#player2 = PlayerCharacter("zbeast", 6)
+
+
+
+#print(player1.age)
+#print(player2.name)
+
+#print(player1.run())
+
+
+
